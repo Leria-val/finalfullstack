875430c067906b203da6d0e6791fd37779d9777c
valeria <valeria_morin@estudante.sesisenai.org.br>
2026-04-30 09:16:23 -0300

    Cohesión de código (padronizandonome das variaveis)

--- backend/src/models/Class.js
+++ backend/src/models/Class.js
@@ -3,25 +3,52 @@ import { sequelize } from "../config/connection.js";
 import User from "./User.js";
 
 const Class = sequelize.define("Class", {
+  id: {
+    type: DataTypes.STRING,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+
+  },
+
   name: {
     type: DataTypes.STRING,
     allowNull: false,
+    validate: { notEmpty: true }
   },
+
   subject: {
-    type: DataTypes.STRING, // Disciplina (ex: Programação FullStack)
+    type: DataTypes.STRING,
     allowNull: false,
+    comment: 'Instrumento ou teoria (ex: Piano, Teoria Musical ||)'
   },
-  teacherId: {
+
+  period: {
+    type: DataTypes.ENUM ('MORNING', 'AFTERNOON', 'NIGHT'),
+    allowNull: false,
+    defaultValue: 'MORNING',
+  },
+
+   room: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    comment: 'Sala física ou linkda sala virtual'
+  },
+
+  teacher_id: {
     type: DataTypes.INTEGER,
+    allowNull: false,
     references: {
       model: User,
       key: "id",
     },
   },
-}, { timestamps: true });
+}, { 
+  underscored: true,
+  timestamps: true 
+});
 
 // Relacionamento: Uma turma pertence a um professor (User)
-Class.belongsTo(User, { foreignKey: "teacherId", as: "teacher" });
-User.hasMany(Class, { foreignKey: "teacherId", as: "classes" });
+Class.belongsTo(User, { foreignKey: "teacher_id", as: "teacher" });
+User.hasMany(Class, { foreignKey: "teacher_id", as: "classes" });
 
 export default Class;
